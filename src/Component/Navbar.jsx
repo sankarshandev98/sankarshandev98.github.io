407a01d52c4dfb0427cc8d124582a2e0434c10b4
sankarshandev98 <gsankar164@gmail.com>
2023-06-09 14:44:07 +0530

    update

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-scroll";
 
 export default function Navbar() {
   const href =
-    "https://drive.google.com/file/d/1Z8I0_ZJFcuqakPRvCgIrs4DpNVSuBT3A/view?usp=sharing";
+    "https://drive.google.com/file/d/1RjoLKLLZqOyWTDDI7lqdFHdXQs3lZkJJ/view?usp=sharing";
   const [nav, setNav] = useState(false);
   const links = [
     {
